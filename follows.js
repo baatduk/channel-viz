ca1a0ae0af902ff8c0eee97cf4379f82d1231e24
baatduk <baatduk@gmail.com>
2015-04-04 19:50:21 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -13,7 +13,7 @@
 	*/
 
 	var defaultKey		= 'Fr6mB8992Oa4subfPqaUdQLpPnSdmPNQOv9U7NZxQOLVDgGD', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [3941936], // Comma separated array of Xively Feed ID numbers
+		defaultFeeds	= '3941936', // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Aeon Home Energy Meter', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
